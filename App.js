ba9ac2e41354becfb11db1b45a078910de3fb891
LeeGeonwoo22 <kwoo.chris@gmail.com>
2022-04-18 16:05:26 +0900

    app.js

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React from 'react';
+import { createStackNavigator} from "@react-navigation/native/stack"
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+const App = () =>{
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
